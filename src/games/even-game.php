4b004187a98470d123b07835809d4ed31a0428d6
Иван Сабиров <sabirivan@yandex.com>
2019-08-24 12:29:30 +1100

    Сделано пятый шаг

---
+++ src/games/even-game.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace BrainGames\Games\EvenGame;
+
+use function BrainGames\Starter\starter;
+
+const MIN_VALUE = 1;
+const MAX_VALUE = 50;
+const TASK = "Answer \"yes\" if number even otherwise answer \"no\"";
+
+function isEven($number)
+{
+  return $number % 2 === 0;
+}
+
+function startGameEven()
+{
+  $generateGame = function() {
+    $question = rand(MIN_VALUE, MAX_VALUE);
+    $rightAnswer = isEven($question) ? "yes" : "no";
+    return [$question, $rightAnswer];
+  };
+  starter(TASK, $generateGame);
+}
---
+++ src/starter.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace BrainGames\Starter;
+
+use function \cli\line;
+use function \cli\prompt;
+
+const ROUNDS_COUNT = 3;
+
+function starter($task, $getQuestionAnswer)
+{
+  line("Welcome to the Brain Games!");
+  line($task);
+  $userName = prompt("May I have your name?");
+  line("Hello, {$userName}!");
+
+  for ($i = 0; $i < ROUNDS_COUNT; $i++) {
+    [$question, $rightAnswer] = $getQuestionAnswer();
+    line("Question: {$question}");
+    $userAnswer = prompt("Your answer");
+
+    if ($userAnswer === $rightAnswer) {
+      line("Correct!");
+    } else {
+      line("'{$userAnswer}' is wrong answer ;(. Correct answer is '{$rightAnswer}'.");
+      line("Let's try again '{$userName}'!");
+      return;
+    }
+  }
+
+  line("Congratulations, {$userName}!");
+}
